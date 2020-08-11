c93a574b79d2c37f022da66f4981696053fa4e34
smslsate <38814256+smslsate@users.noreply.github.com>
2020-08-11 16:39:59 +0300

    Add files via upload

---
+++ build/precache-manifest.5b908020f16273fedb4688b4eb15b52e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e5255a391ea8d0f5f586b413c892d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cee5cecb34cb285100f",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "bf326104debf498c530e",
+    "url": "/static/css/main.d0dbc2b5.chunk.css"
+  },
+  {
+    "revision": "4cee5cecb34cb285100f",
+    "url": "/static/js/2.6c2bf035.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.6c2bf035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf326104debf498c530e",
+    "url": "/static/js/main.1e0e6fe3.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
